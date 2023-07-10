26aa2c5c5e000bc95962097221fdaff8c5aa8c65
Lance <53155359+Lancelof2019@users.noreply.github.com>
2023-07-10 21:43:58 +0200

    Update cook_membrane.cc

--- cook_membrane.cc
+++ cook_membrane.cc
@@ -1,29 +1,3 @@
-/* ---------------------------------------------------------------------
- * Copyright (C) 2010 - 2015 by the deal.II authors and
- *                              Jean-Paul Pelteret and Andrew McBride
- *
- * This file is part of the deal.II library.
- *
- * The deal.II library is free software; you can use it, redistribute
- * it, and/or modify it under the terms of the GNU Lesser General
- * Public License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * The full text of the license can be found in the file LICENSE at
- * the top level of the deal.II distribution.
- *
- * ---------------------------------------------------------------------
- */
-
-/*
- * Authors: Jean-Paul Pelteret, University of Erlangen-Nuremberg,
- *          Andrew McBride, University of Cape Town, 2015, 2017
- */
-
-
-// We start by including all the necessary deal.II header files and some C++
-// related ones. They have been discussed in detail in previous tutorial
-// programs, so you need only refer to past tutorials for details.
-
 #include <unistd.h>
 #include <deal.II/base/function.h>
 #include <deal.II/base/parameter_handler.h>
